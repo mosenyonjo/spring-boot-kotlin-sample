4360e228fbc8ea6eba58c245625da5a861a56f48
Mo <127251605+mosenyonjo@users.noreply.github.com>
2023-04-05 13:05:24 +0100

    Initial Commit for project
    Added class files for Kotlin springboot demo

---
+++ src/main/kotlin/customer/Customer.kt
@@ -0,0 +1,12 @@
+package customer
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+
+@Entity
+class Customer(
+		var firstName: String,
+		var lastName: String,
+		@Id @GeneratedValue
+		var id: Long = -1)
